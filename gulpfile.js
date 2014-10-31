d6839b0f827ef9026b90a6e7e23024c07d443677
Gabe Scholz <gabe@brewhouse.io>
2014-10-30 18:37:49 -0700

    Properly destroy files before watching/building

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,60 @@ var gulp = require('gulp'),
   slim = require('gulp-slim'),
   watch = require('gulp-watch'),
   inject = require('gulp-inject'),
-  plumber = require('gulp-plumber');
+  plumber = require('gulp-plumber'),
+  del = require('del');
 
-var buildPaths = { all: [] };
+var srcPaths = { all: [] };
 ['scss', 'coffee', 'slim'].forEach(function (ext) {
   var paths = ['./src/*.' + ext, './src/**/*.' + ext];
+  srcPaths[ext] = paths;
+  srcPaths.all = srcPaths.all.concat(paths);
+});
+
+var buildPaths = { all: [] };
+['css', 'js', 'html'].forEach(function (ext) {
+  var paths = ['./www/*.' + ext, './www/**/*.' + ext];
   buildPaths[ext] = paths;
   buildPaths.all = buildPaths.all.concat(paths);
 });
+buildPaths.all = buildPaths.all.concat(['!./www/lib/']);
 
-function buildTask (ext, compileStream) {
-  var paths = buildPaths[ext];
+function buildTask (ext, buildExt, compiles) {
+  var paths = srcPaths[ext],
+    build = buildPaths[buildExt];
 
-  gulp.task(ext, function (done) {
-    gulp.src(paths)
-      .pipe(compileStream)
-      .pipe(gulp.dest('./www'))
-      .on('end', done);
+  gulp.task(ext + '-clean', function (done) {
+    del(build, done);
   });
 
-  gulp.task(ext + '-watch', function(done) {
-    gulp.src(paths)
+  gulp.task(ext, [ext + '-clean'], function () {
+    return gulp.src(paths)
+      .pipe(plumber())
+      .pipe(compiles)
+      .pipe(gulp.dest('./www'));
+  });
+
+  gulp.task(ext + '-watch', [ext + '-clean'], function() {
+    return gulp.src(paths)
       .pipe(watch(paths))
       .pipe(plumber())
-      .pipe(compileStream)
-      .pipe(gulp.dest('./www'))
-      .on('end', done);
+      .pipe(compiles)
+      .pipe(gulp.dest('./www'));
   });
 }
 
-buildTask('scss', sass());
-buildTask('coffee', coffee());
-buildTask('slim', slim({ options: ['disable_escape=true', 'logic_less=true'] }));
+buildTask('scss', 'css', sass());
+buildTask('coffee', 'js', coffee());
+buildTask('slim', 'html', slim({ options: ['disable_escape=true', 'logic_less=true'] }));
 
 gulp.task('build', ['scss', 'coffee', 'slim']);
+gulp.task('clean', ['scss-clean', 'coffee-clean', 'slim-clean']);
 gulp.task('watch', ['scss-watch', 'coffee-watch', 'slim-watch']);
+
+gulp.task('inject', function () {
+  return gulp.src('./www/index.html')
+    .pipe(inject(gulp.src(buildPaths.all, { read: false }), {
+      relative: true
+    }))
+    .pipe(gulp.dest('./www'));
+});
